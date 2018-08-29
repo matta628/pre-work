cf5bff6b81686b43d3daf5bac591f86dae8e6620
Matt Aguilar <maguila2@binghamton.edu>
2018-08-28 21:51:10 -0400

    Initial Commit

---
+++ Greek Gods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Greek Gods
+//
+//  Created by Matt on 8/28/18.
+//  Copyright © 2018 Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
