0e2ebd2dd1f933b4aed3b37447d47336205d5ccc
Matt Aguilar <maguila2@binghamton.edu>
2018-09-02 02:42:34 -0400

    Added Greek Gods Files

--- Greek Gods/ViewController.swift
+++ Greek Gods/ViewController.swift
@@ -9,17 +9,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var gold: UIColor!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var odyssey: UIButton!
+    @IBOutlet weak var underworld: UIButton!
+    @IBOutlet weak var rename: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        gold = view.backgroundColor
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func resetView(_ sender: Any) {
+        textLabel.text = "Mount Olympus"
+        textLabel.textColor = UIColor.white
+        odyssey.titleLabel?.textColor = UIColor.white
+        underworld.titleLabel?.textColor = UIColor.white
+        rename.titleLabel?.textColor = UIColor.white
+        view.backgroundColor = gold
+        textField.resignFirstResponder()
+    }
+    
+    @IBAction func changeTxtColor(_ sender: Any) {
+        textLabel.textColor = UIColor.red
+    }
+    @IBAction func changeTheme(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+        odyssey.titleLabel?.textColor = UIColor.red
+        underworld.titleLabel?.textColor = UIColor.red
+        rename.titleLabel?.textColor = UIColor.red
+    }
+    @IBAction func changeText(_ sender: Any) {
+        if (textField.text?.isEmpty)! {
+            textLabel.text = "Mount Othrys"
+            textLabel.textAlignment = NSTextAlignment.center
+        }
+        else {
+            textLabel.text = textField.text
+            textLabel.textAlignment = NSTextAlignment.center
+            textField.text = ""
+            view.endEditing(true)
+        }
+    }
 }
+
+
+
+
 
---
+++ README.md
@@ -0,0 +1,34 @@
+## Greek Gods
+
+### App Description
+    This is a simple app that is Greek Mythology themed
+
+### App Walk-though
+
+    <img src="https://imgflip.com/gif/2h1vvh" width=200><br>
+
+
+### Required User Stories
+    - [x] 1. User sees custom text in a label 
+    - [x] 2. User see's custom background color.
+    - [x] 3. User can tap a button to change the text color of the label.
+
+### Optional User Stories
+    - [x] 1. User can tap a button to change the color of the background view.
+    - [x] 2. User can tap a button to change the text string of the label - Mount Othrys.
+    - [x] 3. User can tap on the background view to reset all views to default settings.
+    - [x] 4. User can update the label text with custom text entered into the text field.
+    - [x] a. User can enter text into a text field using the keyboard.
+    - [ ] b. User can tap the "Change text string" button to update the label with the text from the text field.
+    - [x] c. If the text field is empty, update label with alternate string - Mount Othrys.
+    - [x] d. The keyboard is dismissed after touching the empty space (view).
+    
+### Notes
+    - I do not know why, but the label automatically changes to whatever is typed into the 
+      textField without the Rename button being pressed
+    - I had extreme trouble running the simulator, and I never got it to work. Instead, I had to
+      run the app on my phone, which is led to even more complications on how to make the app 
+      walkthrough gif.
+    - The button that changes the background view also changes the color of two of the buttons,
+      but I could not get it to change the color of the button that is pressed. I do not know why.
+    
